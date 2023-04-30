208307aa487453ab56207b204a0ea0f3c8c5fd5f
AndroPyro28 <menandroeugenio1028@gmail.com>
2023-05-01 01:42:07 +0800

    crud functionality

--- src/components/PostView.tsx
+++ src/components/PostView.tsx
@@ -1,16 +1,123 @@
-import React from 'react';
-import { PostViewWithUser } from '~/types/PostView.types';
-
+import React, { useState } from "react";
+import { PostViewWithUser } from "~/types/PostView.types";
+import {format} from 'timeago.js'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faTrash, faPen} from '@fortawesome/free-solid-svg-icons'
+import { api } from "~/utils/api";
+import { useUser } from "@clerk/nextjs";
 type Props = {
-    postProp: PostViewWithUser
-}
+  postProp: PostViewWithUser;
+};
 
-const PostView = ({postProp} : Props) => {
-    const {author, post} = postProp
-    return <div className="w-full border-b border-slate-400 p-8 flex gap-5" key={post?.id}>
-    <img src={author?.profileImageUrl} alt="" className=" w-10 h-10 rounded-full" />
-    {post?.content}
-  </div>
-}
+const PostView = ({ postProp }: Props) => {
+  const [edit, setEdit] = useState(false);
+  const [editContent, setEditContent] = useState<string>('')
+  const userData = useUser()
+  const {user} = userData
+
+  const utils = api.useContext();
+
+  const { mutate: mutateDelete,} = api.post.delete.useMutation({
+    async onMutate(newPost) {
+      // Cancel outgoing fetches (so they don't overwrite our optimistic
+      await utils.post.getAll.cancel();
+      // Get the data from the queryCache
+      const prevData = utils.post.getAll.getData();
+      // Optimistically update the data with our new post
+      // utils.post.getAll.setData(undefined, (oldPosts) => [...oldPosts?.filter(post => post.post.id != postProp.post.id)]);
+      // Return the previous data so we can revert if something goes wrong
+      return { prevData };
+    },
+    onError(err, newPost, ctx) {
+      utils.post.getAll.setData(undefined, ctx?.prevData);
+    },
+    onSettled() {
+      utils.post.getAll.invalidate();
+    },
+  });
+
+  const { mutate: mutateEdit ,} = api.post.update.useMutation({
+    async onMutate(newPost) {
+      // Cancel outgoing fetches (so they don't overwrite our optimistic
+      await utils.post.getAll.cancel();
+      // Get the data from the queryCache
+      const prevData = utils.post.getAll.getData();
+      // Optimistically update the data with our new post
+      // utils.post.getAll.setData(undefined, (oldPosts) => [...oldPosts?.filter(post => post.post.id != postProp.post.id)]);
+      // Return the previous data so we can revert if something goes wrong
+      return { prevData };
+    },
+    onError(err, newPost, ctx) {
+      utils.post.getAll.setData(undefined, ctx?.prevData);
+    },
+    onSettled() {
+      utils.post.getAll.invalidate();
+    },
+  });
+
+  
+  const { author, post } = postProp;
+
+  const handleDeletePost = () => {
+    const consent = window.confirm('are you sure you to delete this post?')
+
+    if(consent) mutateDelete (post.id)
+  }
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      setEditContent(prev => e.target.value)
+  }
+
+  const handleUpdatePost = () => {
+
+    if(!edit) {
+      setEdit(true)
+    } else {
+      const consent = window.confirm('are you sure you to update this post?')
+
+      if(consent && editContent) {
+        mutateEdit({
+          content: editContent,
+          postId: post.id
+        })
+      }
+      setEdit(false)
+    }
+    
+    
+  }
+
+  const handleEnter = (e: React.KeyboardEvent) => {
+    if (e.key == "Enter") {
+      handleUpdatePost();
+    }
+  };
+
+
+  return (
+    <div className="flex w-full flex-col border-b border-slate-400">
+      <div className="relative flex gap-5 p-8" key={post?.id}>
+
+        <img
+          src={author?.profileImageUrl}
+          alt=""
+          className="h-10 w-10 rounded-full"
+        />
+        <div> {edit ? <input value={editContent} placeholder="update then enter" className="rounded-md text-black px-3 outline-none border bg-slate-200 w-full h-full" onChange={handleChange} onKeyDown={handleEnter}  /> : post?.content }</div>
+
+        {
+          userData.isSignedIn && author.id === user?.id &&  <>
+          <FontAwesomeIcon icon={faPen} className="w-5 h4 absolute right-16 top-2.5 cursor-pointer" onClick={handleUpdatePost}  />
+        <FontAwesomeIcon icon={faTrash} className="w-5 h4 absolute right-5 top-2 cursor-pointer" onClick={handleDeletePost}  />
+          </>
+        }
+
+        <div className=" absolute right-2 bottom-2">
+          {format(post.createdAt)}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default PostView;
